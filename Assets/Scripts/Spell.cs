bb78bf38f06d4113f873c10ec6a36d8c0f398b6e
NielStevensen <40745561+NielStevensen@users.noreply.github.com>
2019-06-24 20:11:57 +1000

    Basic spell modules and order
    basic spell module with charge and fire stubs added

---
+++ Assets/Scripts/Spell.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using SpellModuleList;
+
+public class Spell : MonoBehaviour
+{
+
+    public string[] Modules;
+    public SpellModuleList list;
+    public int currentModule;
+
+    bool pressed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && !pressed)
+        {
+            print("initiate");
+
+            pressed = true;
+
+            currentModule = 0;
+            //list.StartCoroutine(Modules[currentModule]);
+            list.StartCoroutine(Modules[currentModule]);
+        }
+    }
+}
---
+++ Assets/Scripts/SpellModuleList.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellModuleList : MonoBehaviour
+{
+    public Spell caller;
+    float potency;
+    Collision collisionInfo;
+    IEnumerator Charge()
+    {
+        Debug.Log("Charge");
+
+        yield return new WaitForEndOfFrame();
+
+        float holdTime = 0.0f;
+
+        while (Input.GetButton("Fire1"))
+        {
+            yield return new WaitForEndOfFrame();
+
+            holdTime += Time.deltaTime;
+        }
+
+        holdTime = Mathf.Min(holdTime, 5.0f);
+
+        print(holdTime);
+
+        potency = 0.5f + 1.0f * holdTime / 5.0f;
+
+        print(potency);
+        RunNext();
+    }
+
+    IEnumerator Fire()
+    {
+        Debug.Log("Fire");
+        yield return new WaitForEndOfFrame();
+        RunNext();
+    }
+
+    void RunNext()
+    {
+        if (caller.currentModule + 1 < caller.Modules.Length && caller.Modules[caller.currentModule + 1] != "")
+        {
+            caller.currentModule += 1;
+            StartCoroutine(caller.Modules[caller.currentModule]);
+        }
+    }
+
+}
