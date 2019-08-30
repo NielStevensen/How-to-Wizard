87d0f6f3f10e71415316e502a55d8855f351b1f1
nviengkhou <40777998+nviengkhou@users.noreply.github.com>
2019-08-30 12:35:08 +1000

    Working on null

--- Assets/Scripts/Spell modules/FireController.cs
+++ Assets/Scripts/Spell modules/FireController.cs
@@ -8,20 +8,69 @@ public class FireController : MonoBehaviour
 	[Tooltip("Lifetime of the flame object.")]
 	public float lifetime = 5;
 
+	//Values to handle null
+	private List<NullManager> nullObjects = new List<NullManager>();
+	private List<NullManager> objectsToRemove = new List<NullManager>();
+
     //Setup
     void Start()
     {
         Destroy(gameObject, lifetime);
-    }
+	}
 	
-	//On entering the flame, set flammable objects on fire
-    private void OnTriggerEnter(Collider other)
+	//On entering the flame, set flammable, un-nulled objects on fire
+	private void OnTriggerEnter(Collider other)
     {
 		BurnController bc = other.gameObject.GetComponent<BurnController>();
 
 		if (bc != null)
 		{
-			bc.enabled = true;
+			bool canBurn = true;
+
+			NullManager nm = other.gameObject.GetComponent<NullManager>();
+
+			if(nm != null)
+			{
+				if (nm.IsNulled)
+				{
+					nullObjects.Add(nm);
+
+					canBurn = false;
+				}
+			}
+
+			bc.enabled = canBurn;
 		}
     }
+
+	//Check the null state of objects to see if they can be set on fire
+	private void OnTriggerStay(Collider other)
+	{
+		foreach(NullManager nm in nullObjects)
+		{
+			if (!nm.IsNulled)
+			{
+				other.gameObject.GetComponent<BurnController>().enabled = true;
+
+				objectsToRemove.Add(nm);
+			}
+		}
+
+		foreach(NullManager nm in objectsToRemove)
+		{
+			if (nullObjects.Contains(nm))
+			{
+				nullObjects.Remove(nm);
+			}
+		}
+	}
+
+	//Remove nulled objects from the list to check
+	private void OnTriggerExit(Collider other)
+	{
+		if (nullObjects.Contains(other.GetComponent<NullManager>()))
+		{
+			nullObjects.Remove(other.GetComponent<NullManager>());
+		}
+	}
 }
---
+++ Assets/Scripts/Spell modules/NullManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NullManager : MonoBehaviour
+{
+    //Null state
+    [Tooltip("Whether or not this object is nulled.")]
+    [SerializeField]
+    private bool isNulled = false;
+
+    //Null projection object
+    private GameObject nullProjection;
+	[Tooltip("Null shader.")]
+	public Shader nullShader;
+	[HideInInspector]
+	public bool isProjection = false;
+
+    //Retrieve null state
+    public bool IsNulled
+    {
+        get
+        {
+            return isNulled;
+        }
+    }
+    
+    //Create null projection
+    void Start()
+    {
+		if (isProjection)
+		{
+			return;
+		}
+
+		Rigidbody rb = GetComponent<Rigidbody>();
+		bool kinematicState = false;
+
+		if (rb != null)
+		{
+			kinematicState = rb.isKinematic;
+			rb.isKinematic = true;
+		}
+
+		StartCoroutine(HandleKinematicState(rb, kinematicState));
+
+		nullProjection = Instantiate(gameObject, gameObject.transform.position, gameObject.transform.rotation);
+		nullProjection.transform.localScale *= 1.125f;
+		nullProjection.layer = LayerMask.NameToLayer("Ignore Raycast");
+		nullProjection.transform.SetParent(gameObject.transform);
+		nullProjection.GetComponent<NullManager>().isProjection = true;
+
+        Component[] allComponents = nullProjection.GetComponents<Component>();
+        
+        foreach(Component comp in allComponents)
+        {
+            if(!(comp.GetType() == typeof(Transform) || comp.GetType() == typeof(MeshFilter) || comp.GetType() == typeof(MeshRenderer)))
+            {
+                Destroy(comp);
+            }
+        }
+		
+		foreach (Material mat in nullProjection.GetComponent<MeshRenderer>().materials)
+		{
+			//temp
+			/*mat.shader = Shader.Find("Standard");
+			Color temp = mat.color;
+			temp.a = 0.625f;
+			mat.color = temp;*/
+
+			mat.shader = nullShader;
+		}
+		
+        nullProjection.SetActive(isNulled);
+    }
+    
+	//Stop the projection from breaking physics
+	IEnumerator HandleKinematicState(Rigidbody rb, bool state)
+	{
+		yield return new WaitForEndOfFrame();
+		
+		if (rb != null)
+		{
+			rb.isKinematic = state;
+		}
+	}
+
+    //Invert null state
+    public void InvertNullState()
+    {
+		isNulled = !isNulled;
+
+		nullProjection.SetActive(isNulled);
+	}
+}
