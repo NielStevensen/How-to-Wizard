76da26e90d0ae918cea6013b60d565a5c6221af9
Nielstevensen <Stevennielsen4@gmail.com>
2019-09-02 08:53:14 +1000

    added object spawner

--- Assets/Scripts/World interactions/ObjectSpawner.cs
+++ Assets/Scripts/World interactions/ObjectSpawner.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     [Tooltip("list of spawned objects length determins maximum number")]
-    GameObject[] spawnedObjects;
+    public GameObject spawnPrefab;
+    public GameObject[] spawnedObjects;
+    public float spawnInterval;
+    Coroutine spawningDelay;
+    int index;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,26 @@ public class ObjectSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(spawningDelay == null)
+        {
+            for( int i = 0; i < spawnedObjects.Length; i ++)
+            {
+                Debug.Log(i);
+                if (spawnedObjects[i] == null)
+                {
+                    
+                    index = i;
+                    spawningDelay = StartCoroutine(SpawnDelay());
+                    break;
+                }
+            }
+        }      
+    }
+
+    IEnumerator SpawnDelay()
+    { 
+        yield return new WaitForSeconds(spawnInterval);
+        spawnedObjects[index] = Instantiate(spawnPrefab,transform.position, transform.rotation);
+        spawningDelay = null;
     }
 }
