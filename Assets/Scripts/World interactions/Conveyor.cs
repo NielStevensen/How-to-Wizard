bf995925db7c43f60ac6f1e6da204106dd1a3f91
Nielstevensen <Stevennielsen4@gmail.com>
2019-09-15 12:09:55 +1000

    added conveyor belt prfab

---
+++ Assets/Scripts/World interactions/Conveyor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    [Tooltip("can the conveyor use transfrom to move non rigid body objects")]
+    public bool canUseTransfrom;
+    public float Movespeed;
+
+
+    // Update is called once per frame
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.GetComponent<Rigidbody>() != null)
+        {
+            other.GetComponent<Rigidbody>().velocity = other.GetComponent<Rigidbody>().velocity + (transform.forward * Movespeed);
+        }
+        else if (canUseTransfrom)
+        {
+            transform.Translate(transform.forward * Movespeed);
+        }
+    }
+}
