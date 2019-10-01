79b181c97b54a2aba783efa2ac836ec4e43dcecf
NielStevensen <40745561+NielStevensen@users.noreply.github.com>
2019-10-01 19:26:50 +1000

    added craystal materials and changing meshes

--- Assets/Scripts/Spell crafting/CrystalInfo.cs
+++ Assets/Scripts/Spell crafting/CrystalInfo.cs
@@ -8,8 +8,17 @@ public class CrystalInfo : MonoBehaviour
     public int moduleType;
 	public int moduleIndex;
     public bool unused = true;
+    public Material[] materials;
+    public Mesh[] meshes;
 
-	//Used for pc crafting outlines
-	[HideInInspector]
+
+    //Used for pc crafting outlines
+    [HideInInspector]
 	public bool isSelected = false;
+
+    void Start()
+    {
+        GetComponent<MeshFilter>().mesh = meshes[moduleType];
+        GetComponent<Renderer>().material = materials[moduleType];
+    }
 }
