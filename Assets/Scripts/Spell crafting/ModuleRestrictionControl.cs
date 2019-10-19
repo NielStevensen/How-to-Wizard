dedca440b91b0c040298c2081cf284fa6c5cfb58
Nielstevensen <Stevennielsen4@gmail.com>
2019-10-19 12:20:53 +1100

    added basic module restrictor

---
+++ Assets/Scripts/Spell crafting/ModuleRestrictionControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleRestrictionControl : MonoBehaviour
+{
+    public GameObject[] crystals;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Info.currentGameMode == GameMode.NewGamePlus) // activae all for new game plus
+        {
+            foreach(GameObject a in crystals)
+            {
+                a.SetActive(true);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
